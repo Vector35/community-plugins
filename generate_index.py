5cbf212b2900b38916c6af64cb338c494ca37a3a
Jordan Wiens <jordan@psifertex.com>
2022-02-21 18:19:25 -0500

    fix generator in cases where release ordering from API is incorrect

--- generate_index.py
+++ generate_index.py
@@ -46,7 +46,7 @@ def getPluginJson(plugin):
     if 'auto_update' in plugin and plugin['auto_update']:
         releaseList = f"{projectUrl}/releases"
         try:
-            releaseData = getfile(releaseList).json()[0]
+            releaseData = sorted(getfile(releaseList).json(), key=lambda x: parser.parse(x['published_at']), reverse=True)[0]
             if "message" in releaseData and releaseData["message"] == "Not Found":
                 print(f"\n\nERROR: {plugin['name']}, Couldn't get release information. Likely the user created a tag but no associated release.\n")
                 return None
