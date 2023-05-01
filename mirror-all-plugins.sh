1e4a14c7b08f7060e4900bee8704910119c672fe
Jordan Wiens <jordan@psifertex.com>
2023-05-01 14:45:39 -0400

    add script to mirror all plugins locally

---
+++ mirror-all-plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DOWNLOAD_DIR="mirror"
+if [ ! -d $DOWNLOAD_DIR ]
+then
+  mkdir $DOWNLOAD_DIR
+fi
+for plugin in `grep '"name"' listing.json|awk -F\" '{print $4}'`
+do
+  git -C $DOWNLOAD_DIR clone https://github.com/$plugin
+done
