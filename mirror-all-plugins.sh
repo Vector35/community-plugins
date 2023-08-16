7242b0b15c82a46ec5e4757adc994e6d882f12df
Jordan Wiens <jordan@psifertex.com>
2023-08-16 18:12:02 -0400

    add submodule update to mirror script

--- mirror-all-plugins.sh
+++ mirror-all-plugins.sh
@@ -7,4 +7,7 @@ fi
 for plugin in `grep '"name"' listing.json|awk -F\" '{print $4}'`
 do
   git -C $DOWNLOAD_DIR clone https://github.com/$plugin
+  pushd $DOWNLOAD_DIR
+  git submodule update --init --recursive
+  popd
 done
