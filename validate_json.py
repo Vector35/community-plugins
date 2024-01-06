8453d61240b7075f262afd8350545a04f2ce5057
Jordan Wiens <jordan@psifertex.com>
2024-01-05 22:49:06 -0500

    handle both cases for repo urls

--- validate_json.py
+++ validate_json.py
@@ -13,34 +13,46 @@ def getfile(url):
 issue_content = os.environ.get("ISSUE_CONTENT")
 
 lines = issue_content.split("\n")
+
+def validate_repo(line):
+    repo = line.split(" ")[-1]
+    if repo.startswith("https://github"):
+        repo = repo[19:] # strip off beginning of URL if it exists
+    repo = repo.strip().strip("/") # normalize path
+    projectUrl = f"https://api.github.com/repos/{repo}"
+    latestRelease = f"{projectUrl}/releases/latest"
+    tagsUrl = f"{projectUrl}/tags"
+    try:
+        releaseData = getfile(latestRelease).json()
+
+        match releaseData.get('message'):
+            case 'Not Found':
+                print(f"\n\nERROR: {plugin['name']}, Couldn't get release information. Likely the user created a tag but no associated release.\n")
+                sys.exit(-1)
+            case 'Bad credentials':
+                print("\n\nERROR: Bad credentials, check access token.\n")
+                sys.exit(-1)
+
+    except:
+        print(f"\n\nFailed to load valid release data json from {latestRelease}")
+        sys.exit(-1)
+    try:
+        tag = releaseData['tag_name']
+        pluginjsonurl = f"{projectUrl}/contents/plugin.json?ref={tag}"
+        content = getfile(pluginjsonurl).json()['content']
+        jsoncontent = json.loads(base64.b64decode(content))
+    except:
+        print(f"\n\nFailed to parse valid plugin.json from https://github.com/{repo}/blob/master/plugin.json")
+        sys.exit(-1)
+    sys.exit(0)
+
 for line in lines:
     if line.startswith("Repo URL:"):
-        repo = line.split(" ")[-1]
-        if repo.startswith("https://github"):
-            repo = repo[19:] # strip off beginning of URL if it exists
-        repo = repo.strip().strip("/") # normalize path
-        projectUrl = f"https://api.github.com/repos/{repo}"
-        latestRelease = f"{projectUrl}/releases/latest"
-        tagsUrl = f"{projectUrl}/tags"
-        try:
-            releaseData = getfile(latestRelease).json()
+        validate_repo(line)
 
-            match releaseData.get('message'):
-                case 'Not Found':
-                    print(f"\n\nERROR: {plugin['name']}, Couldn't get release information. Likely the user created a tag but no associated release.\n")
-                    sys.exit(-1)
-                case 'Bad credentials':
-                    print("\n\nERROR: Bad credentials, check access token.\n")
-                    sys.exit(-1)
+# Failed to find a repo line, just look for the first github URL:
 
-        except:
-            print(f"\n\nFailed to load valid release data json from {latestRelease}")
-            sys.exit(-1)
-        try:
-            tag = releaseData['tag_name']
-            pluginjsonurl = f"{projectUrl}/contents/plugin.json?ref={tag}"
-            content = getfile(pluginjsonurl).json()['content']
-            jsoncontent = json.loads(base64.b64decode(content))
-        except:
-            print(f"\n\nFailed to parse valid plugin.json from https://github.com/{repo}/blob/master/plugin.json")
-            sys.exit(-1)
+for line in lines:
+    if line.startswith("https://github.com/"):
+        validate_repo(line)
+
