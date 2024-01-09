7778086ee38d27ba574b7abc415307db655e82f0
Jordan Wiens <jordan@psifertex.com>
2024-01-09 14:19:07 -0500

    add discord presence

--- mirror-all-plugins.sh
+++ mirror-all-plugins.sh
@@ -6,8 +6,12 @@ then
 fi
 for plugin in `grep '"name"' listing.json|awk -F\" '{print $4}'`
 do
-  git -C $DOWNLOAD_DIR clone https://github.com/$plugin
-  pushd $DOWNLOAD_DIR
+  dirname=`echo $plugin|sed 's/.*\///g'`
+  if [ ! -d $DOWNLOAD_DIR/$dirname ]
+  then
+    git -C $DOWNLOAD_DIR clone https://github.com/$plugin
+  fi
+  pushd $DOWNLOAD_DIR/$dirname
   git submodule update --init --recursive
   popd
 done
